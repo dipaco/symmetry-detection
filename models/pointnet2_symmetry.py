52bf3d4c8b62bb7380d806a08e063c7933550970
Diego Patino <dipaco@gmail.com>
2019-08-19 18:03:18 -0400

    restoring shape calculation

--- models/pointnet2_symmetry.py
+++ models/pointnet2_symmetry.py
@@ -22,8 +22,8 @@ def get_model(next_element, is_training, bn_decay=None):
     next_face_normals, next_triangles, point_cloud, next_gt_planes, next_w, next_num_planes, next_filename = next_element
 
 
-    batch_size = tf.shape(point_cloud)[0]
-    num_point = tf.shape(point_cloud)[1]
+    batch_size = point_cloud.get_shape()[0].value
+    num_point = point_cloud.get_shape()[1].value
     end_points = {}
     l0_xyz = point_cloud
     l0_points = None
