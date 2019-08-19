10f779785d1d710b1f8dd66b357dd55f1d421e52
Diego Patino <dipaco@gmail.com>
2019-08-19 14:59:09 -0400

    restoring the save interval

--- train.py
+++ train.py
@@ -195,10 +195,10 @@ def train():
             eval_one_epoch(sess, ops, test_writer)
 
             # Save the variables to disk.
-            #if epoch % 10 == 0:
-            tf.assign(epoch_var, epoch + 1)
-            save_path = saver.save(sess, os.path.join(LOG_DIR, 'checkpoint'), global_step=batch)
-            log_string("Model saved in file: %s" % save_path)
+            if epoch % 10 == 0:
+                tf.assign(epoch_var, epoch + 1)
+                save_path = saver.save(sess, os.path.join(LOG_DIR, 'checkpoint'), global_step=batch)
+                log_string("Model saved in file: %s" % save_path)
 
 
 def train_one_epoch(sess, ops, train_writer):
