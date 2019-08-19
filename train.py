692564d3a38588e79c30152cdecacefaebb98337
Diego Patino <dipaco@gmail.com>
2019-08-19 14:49:01 -0400

    using abs path to load files

--- train.py
+++ train.py
@@ -153,7 +153,7 @@ def train():
 
         # if a checkpoint exists, restore from the latest checkpoint
         epoch_var = tf.Variable(0, trainable=False, name='epoch', dtype=tf.int32)
-        ckpt = tf.train.get_checkpoint_state(os.path.join(LOG_DIR, 'model.ckpt'))
+        ckpt = tf.train.get_checkpoint_state(os.path.abspath(os.path.join(LOG_DIR, 'model.ckpt')))
         print('----', os.path.abspath(os.path.join(LOG_DIR, 'model.ckpt')), ckpt)
         if ckpt and ckpt.model_checkpoint_path:
             print('----- restoring model')
