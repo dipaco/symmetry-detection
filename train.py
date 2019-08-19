21910d5dbb71320d6690f87068f703a9a2e0400a
Diego Patino <dipaco@gmail.com>
2019-08-19 13:59:09 -0400

    fixing checkpoitn loading

--- train.py
+++ train.py
@@ -153,7 +153,7 @@ def train():
 
         # if a checkpoint exists, restore from the latest checkpoint
         epoch_var = tf.Variable(0, trainable=False, name='epoch', dtype=tf.int32)
-        ckpt = tf.train.get_checkpoint_state(os.path.join(LOG_DIR, 'checkpoint'))
+        ckpt = tf.train.get_checkpoint_state(os.path.abspath(os.path.join(LOG_DIR, 'checkpoint')))
         print('----', LOG_DIR, ckpt)
         if ckpt and ckpt.model_checkpoint_path:
             print('----- restoring model')
