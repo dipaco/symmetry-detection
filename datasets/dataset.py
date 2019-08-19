96c8a5b1906b1d0e692bfffa1fd6da107d7dc10e
Diego Patino <dipaco@gmail.com>
2019-08-19 18:31:32 -0400

    setting the batch size in the dataset

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -59,16 +59,21 @@ class Dataset(object):
         dataset = dataset.map(map_func=map_function)
         #dataset = dataset.filter(
         #    lambda w, x, y, z, u, v, f: tf.less_equal(tf.shape(w)[0], tf.cast(FLAGS.max_mesh_size, tf.int32)))
-        dataset = dataset.map(map_func=lambda w, x, y, z, u, v, f: [w,
+        '''dataset = dataset.map(map_func=lambda w, x, y, z, u, v, f: [w,
                                                                     tf.transpose(x, perm=[0, 2, 1]),
                                                                     y,
                                                                     z,
                                                                     u,
                                                                     v,
-                                                                    [f]])
+                                                                    [f]])'''
+        dataset = dataset.map(map_func=lambda w, x, y, z, u, v, f: [y,
+                                                                            z,
+                                                                            u,
+                                                                            v,
+                                                                            [f]])
         # dataset = dataset.batch(1)
-        dataset = dataset.padded_batch(1, ([None, 3], [None, 3, 3], [1024, 3], [3, 3, 3], [3], [], [1]))
-        # dataset = dataset.batch(1)
+        #dataset = dataset.padded_batch(1, ([None, 3], [None, 3, 3], [1024, 3], [3, 3, 3], [3], [], [1]))
+        dataset = dataset.batch(FLAGS.batch_size)
         # dataset = dataset.shard(num_shards, 0)
         # dataset = dataset.apply(tf.data.experimental.filter_for_shard(num_shards, 0))
 
