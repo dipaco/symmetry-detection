07b16a21d31c778c553a86e4fd58e427243afa49
Diego Patino <dipaco@gmail.com>
2019-08-19 18:07:19 -0400

    getting elements form the iterator

--- models/pointnet2_symmetry.py
+++ models/pointnet2_symmetry.py
@@ -17,10 +17,9 @@ def placeholder_inputs(batch_size, num_point):
     labels_pl = tf.placeholder(tf.int32, shape=(batch_size))
     return pointclouds_pl, labels_pl
 
-def get_model(point_cloud, is_training, bn_decay=None):
+def get_model(next_element, is_training, bn_decay=None):
     """ Classification PointNet, input is BxNx3, output Bx40 """
-    #next_face_normals, next_triangles, point_cloud, next_gt_planes, next_w, next_num_planes, next_filename = next_element
-
+    next_face_normals, next_triangles, point_cloud, next_gt_planes, next_w, next_num_planes, next_filename = next_element
 
     batch_size = point_cloud.get_shape()[0].value
     num_point = point_cloud.get_shape()[1].value
