4d025273b6491ced490eb63a148f3bdaceb5ca12
Diego Patino <dipaco@gmail.com>
2019-10-09 13:47:53 -0400

    updating the place holders for the dataset

--- models/pointnet2_sym.py
+++ models/pointnet2_sym.py
@@ -14,7 +14,7 @@ from pointnet_util import pointnet_sa_module
 
 def placeholder_inputs(batch_size, num_point):
     pointclouds_pl = tf.placeholder(tf.float32, shape=(batch_size, num_point, 3))
-    labels_pl = tf.placeholder(tf.int32, shape=(batch_size))
+    labels_pl = tf.placeholder(tf.int32, shape=(batch_size, 3))
     return pointclouds_pl, labels_pl
 
 def get_model(point_cloud, is_training, bn_decay=None):
